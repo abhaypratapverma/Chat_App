01e333c24294276d40ffa53180d89fad29a1408a
Abhay Pratap Verma <abhaypratapverma6969@gmail.com>
2025-10-19 11:33:03 +0530

    Add socket.io integration and update dependencies

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js'
-import chatRoutes from './routes/chat.js'
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import chatRoutes from "./routes/chat.js";
+import cors from "cors";
+import { app, server } from "./config/socket.js";
 
 dotenv.config();
 connectDb();
 
-const app = express();
+// const app = express();
 app.use(express.json()); // <-- Needed for parsing JSON
-app.use(cors())
-app.use("/api/v1",chatRoutes)
+app.use(cors());
+app.use("/api/v1", chatRoutes);
 
 const port = process.env.PORT || 5002;
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
-})
-
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
